4c8dd6b0ea1fb19d4f6e933b3ffc153993224a0a
linchanglin <493043444@qq.com>
2017-03-16 00:05:13 +0800

    changed profileShowInput

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,13 +5,43 @@ Page({
         userInfo: {}
     },
     onLoad: function () {
-        let that = this;
-
-        app.getUserInfo(function (userInfo) {
-            console.log('uerInfo', userInfo);
-            that.setData({
-                userInfo: userInfo
-            })
-        })
+        wx.login({
+            success: (res) => {
+                const code = res.code
+                if (code) {
+                    wx.getUserInfo({
+                        success: (res) => {
+                            console.log('code',code, 'encryptedData',res.encryptedData, 'iv',res.iv)
+                            return
+                            api.post('signIn', {
+                                code: code,
+                                encryptedData: res.encryptedData,
+                                iv: res.iv
+                            }, (data) => {
+                                console.log(data)
+                                this.setData({
+                                    avatar: data.people.avatar,
+                                    name: data.people.name,
+                                    authed: true
+                                })
+                                try {
+                                    wx.setStorageSync('token', data.token)
+                                    wx.setStorageSync('people', data.people)
+                                } catch (e) {
+                                    console.log(e)
+                                }
+                                wx.showToast({
+                                    title: '登录成功',
+                                    icon: 'success',
+                                    duration: 2000
+                                })
+                            })
+                        }
+                    })
+                } else {
+                    console.log('获取用户登录态失败！' + res.errMsg)
+                }
+            }
+        });
     }
 });
