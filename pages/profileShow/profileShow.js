2b9745592e25cc531271c4dc4c0f27b6c275e16a
linchanglin <493043444@qq.com>
2017-04-17 22:19:24 +0800

    just save

--- pages/profileShow/profileShow.js
+++ pages/profileShow/profileShow.js
@@ -1,10 +1,6 @@
 Page({
   data: {
     signature: "这一秒，无缘无份，只是在某个地点，擦身而过，也无意相互问好，就这样成为彼此生活中的匆匆过客。这一秒，时间指引你我各自离去；这一秒，距离割断思念如梦辗转。",
-    pictures: ['http://tnfs.tngou.net/img/ext/161223/7083a1fde72448a62e477c5aab0721c8.jpg',
-      'http://tnfs.tngou.net/img/ext/161213/c5f1416b4feb857b8d711f83dc692885.jpg',
-      'http://tnfs.tngou.net/img/ext/161209/6cc26c6f440c091e0cf78229a9642929.jpg',
-      'http://tnfs.tngou.net/img/ext/161213/a94ead894d0d0e4e5b3b807626eeab4d.jpg']
   },
   onLoad: function (option) {
     let that = this;
