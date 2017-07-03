0862d143e7b32ee6872fcc84935459dc1eb5dcdf
曾昌林 <changlin.zeng@shatacloud.com>
2017-07-03 18:58:21 +0800

    changed apis for version2

--- pages/board/board.js
+++ pages/board/board.js
@@ -81,7 +81,7 @@ Page({
     get_available: function () {
         let that = this;
         wx.request({
-            url: 'https://collhome.com/apis/get_available',
+            url: 'https://collhome.com/shanghai/apis/get_available',
             success: function (res) {
                 let get_available = res.data.data;
                 that.setData({
